700102be1edc246af9c0bbecaf530cf5e0073daa
iHaveTailsyt <127070806+iHaveTailsyt@users.noreply.github.com>
2023-04-19 10:50:04 -0400

    Add files via upload

--- lexer.ts
+++ lexer.ts
@@ -1,81 +1,130 @@
-
 export enum TokenType {
+	// Literal Types
 	Number,
-	Indentifier,
-	Equals,
-	OpenParen, 
-	CloseParen,
-	BinaryOperator,
+	Identifier,
+
+	// Keywords
 	Let,
+
+	// Grouping * Operators
+	BinaryOperator,
+	Equals,
+	OpenParen,
+	CloseParen,
 }
 
+/**
+ * Constant lookup for keywords and known identifiers + symbols.
+ */
 const KEYWORDS: Record<string, TokenType> = {
-	"let": TokenType.Let,
-}
+	let: TokenType.Let,
+};
 
+// Reoresents a single token from the source-code.
 export interface Token {
-	value: string,
-	type: TokenType,
-
+	value: string; // contains the raw value as seen inside the source code.
+	type: TokenType; // tagged structure.
 }
 
-function token (value = "", type: TokenType) {
-	return { value, type }
-} 
+// Returns a token of a given type and value
+function token(value = "", type: TokenType): Token {
+	return { value, type };
+}
 
-function isalpha (src: string) {
+/**
+ * Returns whether the character passed in alphabetic -> [a-zA-Z]
+ */
+function isalpha(src: string) {
 	return src.toUpperCase() != src.toLowerCase();
 }
 
-function isskippable (str: string) {
-	return str == ' ' || str == '\n' || str == '\t';
+/**
+ * Returns true if the character is whitespace like -> [\s, \t, \n]
+ */
+function isskippable(str: string) {
+	return str == " " || str == "\n" || str == "\t";
 }
 
-function isint (str: string) {
+/**
+ Return whether the character is a valid integer -> [0-9]
+ */
+function isint(str: string) {
 	const c = str.charCodeAt(0);
-	const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
-	return (c >= bounds[0] && c <= bounds[1]);
+	const bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
+	return c >= bounds[0] && c <= bounds[1];
 }
 
-export function tokenize (sourceCode: string): Token[] {
-
+/**
+ * Given a string representing source code: Produce tokens and handles
+ * possible unidentified characters.
+ *
+ * - Returns a array of tokens.
+ * - Does not modify the incoming string.
+ */
+export function tokenize(sourceCode: string): Token[] {
 	const tokens = new Array<Token>();
 	const src = sourceCode.split("");
 
+	// produce tokens until the EOF is reached.
 	while (src.length > 0) {
-		if (src[0] == '(') {
+		// BEGIN PARSING ONE CHARACTER TOKENS
+		if (src[0] == "(") {
 			tokens.push(token(src.shift(), TokenType.OpenParen));
-		} else if (src[0] == ')') {
+		} else if (src[0] == ")") {
 			tokens.push(token(src.shift(), TokenType.CloseParen));
-		} else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+		} // HANDLE BINARY OPERATORS
+		else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
 			tokens.push(token(src.shift(), TokenType.BinaryOperator));
-		} else if (src[0] == "=") {
-			tokens.push(token(src.shift(), TokenType.Equals);
-		} else {
-			if(isint (src[0])) {
+		} // Handle Conditional & Assignment Tokens
+		else if (src[0] == "=") {
+			tokens.push(token(src.shift(), TokenType.Equals));
+		} // HANDLE MULTICHARACTER KEYWORDS, TOKENS, IDENTIFIERS ETC...
+		else {
+			// Handle numeric literals -> Integers
+			if (isint(src[0])) {
 				let num = "";
 				while (src.length > 0 && isint(src[0])) {
 					num += src.shift();
 				}
 
+				// append new numeric token.
 				tokens.push(token(num, TokenType.Number));
-
-			} else if (isalpha(src[0])) {
+			} // Handle Identifier & Keyword Tokens.
+			else if (isalpha(src[0])) {
 				let ident = "";
 				while (src.length > 0 && isalpha(src[0])) {
 					ident += src.shift();
 				}
+
+				// CHECK FOR RESERVED KEYWORDS
 				const reserved = KEYWORDS[ident];
-				if (reserved == undefined ) {
-					tokens.push(token(ident, TokenType.Indentifier));
-				} else {
+				// If value is not undefined then the identifier is
+				// reconized keyword
+				if (reserved) {
 					tokens.push(token(ident, reserved));
+				} else {
+					// Unreconized name must mean user defined symbol.
+					tokens.push(token(ident, TokenType.Identifier));
 				}
+			} else if (isskippable(src[0])) {
+				// Skip uneeded chars.
+				src.shift();
+			} // Handle unreconized characters.
+			// TODO: Impliment better errors and error recovery.
+			else {
+				console.error(
+					"Unreconized character found in source: ",
+					src[0].charCodeAt(0),
+					src[0]
+				);
+				Deno.exit(1);
 			}
-		}	
+		}
 	}
 
-
-
 	return tokens;
+}
+const source = await Deno.readTextFile("./lexer.ts");
+for (const token of tokenize(source)) {
+	console.log(token);
 }
